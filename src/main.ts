97b6d2368033eea4c54aa0d69de1dbf343d51df7
Paul PLANCQ <paul.plancq@outlook.fr>
2025-04-30 15:31:29 +0200

    feat: add git repository initialization during project setup

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { getPromptArgs } from '@/step/getPromptArgs';
+import { initGitRepository } from '@/step/initGitRepository';
 import { initRepository } from '@/step/initRepository';
 import { installPackages } from '@/step/installPackages';
 import { prepareRepository } from '@/step/prepareRepository';
-import { intro } from '@clack/prompts';
+import { intro, log, note, outro } from '@clack/prompts';
 
 export const main = async () => {
   intro('Create React App With Axa Design System');
 
-  const { projectName, designSystem } = await getPromptArgs();
+  const { projectName, designSystem, enableGit } = await getPromptArgs();
 
   const { projectPath } = initRepository(projectName);
 
@@ -15,7 +16,18 @@ export const main = async () => {
 
   await installPackages({ projectPath, designSystem });
 
-  console.info(`You projectName: ${projectName}`);
-  console.info(`You designSystem: ${designSystem}`);
-  console.info(`You projectPath: ${projectPath}`);
+  if (enableGit) {
+    await initGitRepository(projectPath);
+  }
+
+  log.success(`Success \\o/ Created ${projectName} at ${projectPath}`);
+  note(`Inside that directory, you can run several commands:
+ npm start    Starts the development server.
+ npm build    Bundles the app into static files for production.
+
+We suggest that you begin by typing:
+ cd ${projectName}
+ npm start`);
+
+  outro('Happy hacking!');
 };
