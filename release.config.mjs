1e6fef326f5d0d2a64ef953669903d978aba3d72
Paul PLANCQ <paul.plancq@outlook.fr>
2025-05-15 16:13:37 +0200

    ci: add release workflow and configuration for semantic release

---
+++ release.config.mjs
@@ -0,0 +1,53 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'master',
+    'main',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true },
+  ],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+        writerOpts: {
+          commitsSort: ['scope', 'subject'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    ['@semantic-release/npm'],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+
+        message: 'chore(release): ${nextRelease.name} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    ['@semantic-release/github'],
+  ],
+};
