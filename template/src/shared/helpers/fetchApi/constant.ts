edf325080f9e0068ce9e0f2938bf2fa4a235117f
Paul PLANCQ <paul.plancq@outlook.fr>
2025-05-23 09:40:32 +0200

    feat: implement fetchApi function with error handling and response parsing

---
+++ template/src/shared/helpers/fetchApi/constant.ts
@@ -0,0 +1,16 @@
+export const METHODS = Object.freeze({
+  get: 'GET',
+  post: 'POST',
+  put: 'PUT',
+  patch: 'PATCH',
+  delete: 'DELETE',
+});
+
+export const HEADERS = Object.freeze({
+  contentType: 'content-type',
+});
+
+export const MIME_TYPES = Object.freeze({
+  json: 'application/json',
+  text: 'plain/text',
+});
